f582af46f9c9d58257f1efcc4d26a44c7808ae2b
luorixo <luorixo@gmail.com>
2023-06-28 18:12:01 +1200

    finished lesson5-transform-objects

--- 05-transform-object/src/script.js
+++ 05-transform-object/src/script.js
@@ -9,10 +9,36 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group();
+group.position.y = 0.2;
+group.scale.y = 1.8;
+group.rotation.y = 1;
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+cube1.position.set(-1.5, 0, 0);
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+cube2.position.set(0, 0, 0);
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+cube3.position.set(1.5, 0, 0);
+group.add(cube3);
+
+// axes helper
+const axesHelper = new THREE.AxesHelper(1);
+scene.add(axesHelper);
 
 /**
  * Sizes
@@ -27,6 +53,8 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+// camera.position.x = 1
+// camera.position.y = 1
 scene.add(camera)
 
 /**
